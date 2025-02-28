d039a88fbc656fb08de3c15af52a3f4132a5d8c3
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-02-28 13:07:42 +0100

    Add Header to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-function App() {
-  return (
-    <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
-    </>
-  );
+import Header from "./components/Header.jsx";
+import Meals from "./components/Meals.jsx";
+
+function App({meals}) {
+    return (
+        <>
+            <Header/>
+        </>
+    );
 }
 
 export default App;
