2ab0c02ebc9280ecf5bc16d57e6f610e6f9479bb
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-03-01 09:51:48 +0100

    Display total cart items in Header using CartContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import logo from '../assets/logo.jpg';
 import Button from "./Button.jsx";
+import {useContext} from "react";
+import {CartContext} from "./CartContext.jsx";
 
 export default function Header() {
+    const cartCtx = useContext(CartContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
+
     return (
         <header id="main-header">
             <div id='title'>
@@ -9,7 +17,7 @@ export default function Header() {
                 <h1>ReactFood</h1>
             </div>
             <nav>
-                <Button textOnly>Cart (0)</Button>
+                <Button textOnly>Cart ({totalCartItems})</Button>
             </nav>
         </header>
     )
