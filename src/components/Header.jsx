083bb944d1f7dbe6c9c9c285a28309a05fc7fe2a
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-03-07 12:31:31 +0100

    Integrate UserProgressContext to handle cart visibility in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
 import logo from '../assets/logo.jpg';
-import Button from "./Button.jsx";
+import Button from "../UI/Button.jsx";
 import {useContext} from "react";
-import {CartContext} from "./CartContext.jsx";
+import {CartContext} from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Header() {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
 
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return totalNumberOfItems + item.quantity;
     }, 0);
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
 
     return (
         <header id="main-header">
@@ -17,7 +23,7 @@ export default function Header() {
                 <h1>ReactFood</h1>
             </div>
             <nav>
-                <Button textOnly>Cart ({totalCartItems})</Button>
+                <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
             </nav>
         </header>
     )
