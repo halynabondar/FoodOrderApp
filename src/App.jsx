24f94b4d8cd15aeb19a7b881811aee80534a7a28
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-03-07 12:34:04 +0100

    Add UserProgressContext and integrate Cart & Checkout components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Header from "./components/Header.jsx";
 import Meals from "./components/Meals.jsx";
-import CartContextProvider from "./components/CartContext.jsx";
+import CartContextProvider from "./store/CartContext.jsx";
+import {UserProgressContextProvider} from "./store/UserProgressContext.jsx";
+import Cart from "./components/Cart.jsx";
+import Checkout from "./components/Checkout.jsx";
 
 function App() {
     return (
-        <CartContextProvider>
-            <Header/>
-            <Meals/>
-        </CartContextProvider>
+        <UserProgressContextProvider>
+            <CartContextProvider>
+                <Header/>
+                <Meals/>
+                <Cart />
+                <Checkout />
+            </CartContextProvider>
+        </UserProgressContextProvider>
     );
 }
 
